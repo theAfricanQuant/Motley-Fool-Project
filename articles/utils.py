761123047319a79f791ecb10863bd40e2e9e0779
Ricky White <rickywhite@outlook.com>
2019-04-01 11:44:48 -0400

    Added fucnction to utils.py to fetch 3 random articles.

--- articles/utils.py
+++ articles/utils.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -9,7 +10,7 @@ CONTENT_API = os.path.join(BASE_DIR, "api_files/content_api.json")
 
 def find_headline_article():
     """
-    Finds all the articles in the content_api file with the tag slug="10=promise" and appends into
+    Finds all the articles in the content_api file with the tag slug='10=promise' and appends into
      a new list. The first artilc in the list is returned when the fuction is called.
     """
 
@@ -26,4 +27,20 @@ def find_headline_article():
         return matched_articles[0]
 
 
-find_headline_article()
+def find_random_three_articles():
+    """
+    Returns a list of 3 random results (articles) from the provided content_api.json fle.
+    """
+    with open(CONTENT_API, "r") as content:
+        data = json.load(content)
+
+        random_articles = []
+        articles = data['results']
+
+        while len(random_articles) != 3:
+            rand = random.choice(articles)
+            if rand not in random_articles:
+                random_articles.append(rand)
+
+        return random_articles
+
