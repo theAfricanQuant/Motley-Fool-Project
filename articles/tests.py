484a23e803e613cf9c839b58531cc75850a643a7
Ricky White <rickywhite@outlook.com>
2019-04-01 10:26:07 -0400

    Tests added - Template status tests

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TemplateTests(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_articles_page_status_code(self):
+        response = self.client.get('/articles/')
+        self.assertEqual(response.status_code, 200)
+
