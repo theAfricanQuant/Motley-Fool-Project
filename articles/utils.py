48243c623651f42f35c8fa817543111d58952b95
Ricky White <rickywhite@outlook.com>
2019-04-01 11:22:41 -0400

    Added a util.py for helper function related to the project assessment criteria. First func added.

---
+++ articles/utils.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CONTENT_API = os.path.join(BASE_DIR, "api_files/content_api.json")
+
+
+def find_headline_article():
+    """
+    Finds all the articles in the content_api file with the tag slug="10=promise" and appends into
+     a new list. The first artilc in the list is returned when the fuction is called.
+    """
+
+    with open(CONTENT_API, "r") as content:
+        data = json.load(content)
+
+        matched_articles = []
+
+        for result in data["results"]:
+            for tag in result["tags"]:
+                if tag["slug"] == "10-promise":
+                    matched_articles.append(result)
+
+        return matched_articles[0]
+
+
+find_headline_article()
