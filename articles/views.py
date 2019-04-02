d1c06fee07ac7b83f0e437918bb742b2ab99d29c
Ricky White <rickywhite@outlook.com>
2019-04-01 23:08:36 -0400

    Added content for extra articles and added context in view

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from .utils import find_headline_article
+from .utils import find_headline_article, find_random_three_articles
 
 
 class HomePageView(TemplateView):
@@ -10,7 +10,9 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         main_article = find_headline_article()
+        extra_articles = find_random_three_articles()
         context['main_article'] = main_article
+        context['random_articles'] = extra_articles
         return context
 
 
