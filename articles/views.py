3479accf8bc3fe84c9093d3a0ecb0cd37b88ed04
Ricky White <rickywhite@outlook.com>
2019-04-01 22:47:43 -0400

    added main article to template from context

--- articles/views.py
+++ articles/views.py
@@ -10,7 +10,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         main_article = find_headline_article()
-        context['main-article'] = main_article
+        context['main_article'] = main_article
         return context
 
 
