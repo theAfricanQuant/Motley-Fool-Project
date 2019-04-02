84a2a215802d08739c90ae2eef02f6a79f15f3b5
Ricky White <rickywhite@outlook.com>
2019-04-01 22:00:30 -0400

    Added context to homepage

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,17 @@
 # from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .utils import find_headline_article
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        main_article = find_headline_article()
+        context['main-article'] = main_article
+        return context
 
 
 class ArticlesPageView(TemplateView):
