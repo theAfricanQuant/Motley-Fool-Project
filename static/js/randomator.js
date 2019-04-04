8d43ee269f89645e83874e1c2c706ddcf9d65c18
Ricky White <rickywhite@outlook.com>
2019-04-04 17:03:30 -0400

    Comments added. Formatted with prettier.

--- static/js/randomator.js
+++ static/js/randomator.js
@@ -1,12 +1,23 @@
 // Randomization of the Quotes in the Sidbar of article pages
 
 function rearrangeQuotes() {
+  // Select all elements with the class of 'quote'
   var quotes = $(".quote");
+
+  // Loop through the quotes
   for (var i = 0; i < quotes.length; i++) {
+    /*
+      Randomly generates a number between 0-3 (three being the lenght of the array)
+      Does some of the random math and assigns to two variables.
+      Occasionally these are the same results due to the small array length and nothing changes.
+    */
     var target = Math.floor(Math.random() * quotes.length - 1) + 1;
-    var target2 = Math.floor(Math.random() * quotes.length - 2) + 1;
+    var target2 = Math.floor(Math.random() * quotes.length - 1) + 1;
+
+    // Uses the jQuery libray eq() -- https://api.jquery.com/eq/
     quotes.eq(target).before(quotes.eq(target2));
   }
 }
 
+// Event listener for the RANDOM-ATOR button click!
 document.addEventListener("click", rearrangeQuotes);
