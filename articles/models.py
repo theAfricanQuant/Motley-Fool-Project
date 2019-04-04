55dcf62dd80934290d1dab12e59ea8cdbd9bf460
Ricky White <rickywhite@outlook.com>
2019-04-03 21:37:40 -0400

    Comment Model created

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=500, blank=False)
+    datetime = models.DateTimeField(auto_now_add=True)
+    article_uuid = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.comment[:50]
