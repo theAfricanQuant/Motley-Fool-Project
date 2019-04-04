827fbb2ce063d3df6d72817176feb36aed12ad1d
Ricky White <rickywhite@outlook.com>
2019-04-03 21:39:24 -0400

    Comment Form added to the template and validation added to view.py

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
 # from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from django.shortcuts import HttpResponseRedirect
 
+from .models import Comment
+from .forms import CommentForm
 from .utils import (
     find_headline_article,
     find_random_three_articles,
@@ -21,14 +25,29 @@ class HomePageView(TemplateView):
         return context
 
 
-class ArticleDetailPage(TemplateView):
+class ArticleDetailPage(FormView):
     template_name = "article.html"
+    model = Comment
+    form_class = CommentForm
+    success_url = reverse_lazy("homepage")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         uuid = self.kwargs["article_uuid"]
+
         article = fetch_single_article(str(uuid))
         context["article"] = article
+
         quotes = find_three_random_quotes()
         context["quotes"] = quotes
+
         return context
+
+    def form_valid(self, form):
+        comment = form.cleaned_data["comment"]
+        uuid = form.cleaned_data["article_uuid"]
+
+        new_comment = Comment(comment=comment, article_uuid=uuid)
+        new_comment.save()
+
+        return HttpResponseRedirect(self.get_success_url())
