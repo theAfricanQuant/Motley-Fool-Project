0b7f2af7cf8da8061afd653589b525dbe9d2d4d2
Ricky White <rickywhite@outlook.com>
2019-04-04 16:54:45 -0400

    Comments added to Util methods and Views

--- articles/utils.py
+++ articles/utils.py
@@ -19,10 +19,14 @@ def find_headline_article():
     with open(CONTENT_API, "r", encoding="utf8") as content:
         data = json.load(content)
 
+        # Empty list for articles with slug=10-promise to be stored
         matched_articles = []
 
+        # Loop through the articles in the content_api file
         for result in data["results"]:
+            # Search thorugh articles that have multiple tags
             for tag in result["tags"]:
+                # Find matching slug
                 if tag["slug"] == "10-promise":
                     uuid = result["uuid"]
                     images = result["images"]
@@ -31,6 +35,7 @@ def find_headline_article():
                     promo = result["promo"]
                     date = datetime.datetime.strptime(result["modified"], "%Y-%m-%dT%H:%M:%SZ")
 
+                    # Add the useful properties for each article into a dictionary
                     article = {
                         "images": images,
                         "title": title,
@@ -40,8 +45,10 @@ def find_headline_article():
                         "uuid": uuid,
                     }
 
+                    # Append the created dictionary into the list declared above
                     matched_articles.append(article)
 
+        # Returns the first item of the list only
         return matched_articles[0]
 
 
@@ -74,9 +81,11 @@ def find_random_three_articles():
                 "uuid": uuid,
             }
 
+            # Check to make sure the article isn't alredy in the list
             if article not in random_articles:
                 random_articles.append(article)
 
+        # Returns the list of 3 articles
         return random_articles
 
 
@@ -111,6 +120,7 @@ def find_three_random_quotes():
             price_change = rand["Change"]["Amount"]
             percentage_change = rand["PercentChange"]["Value"]
 
+            # Create a dictionary of needed values per quote
             quote = {
                 "close": close_price,
                 "change": price_change,
@@ -119,7 +129,9 @@ def find_three_random_quotes():
                 "exchange": exchange,
             }
 
+            # Check to make sure the quote isn't alredy in the list
             if quote not in random_quotes:
                 random_quotes.append(quote)
 
+        # Returns 3 random quotes in a list
         return random_quotes
--- articles/views.py
+++ articles/views.py
@@ -19,12 +19,15 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # Returns the hightlighed article at the top of the homepage
         main_article = find_headline_article()
         context["main_article"] = main_article
 
+        # Returns 3 random articles to populate the rest of the homepage
         extra_articles = find_random_three_articles()
         context["random_articles"] = extra_articles
 
+        # Return all context
         return context
 
 
@@ -36,27 +39,38 @@ class ArticleDetailPage(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        # Captures the articles UUID from the url
         uuid = self.kwargs["article_uuid"]
 
+        # Returns the selected article content for context
         article = fetch_single_article(str(uuid))
         context["article"] = article
 
+        # Returns random articles for the sidebar
         extra_articles = find_random_three_articles()
         context["random_articles"] = extra_articles
 
+        # Returns the 3 random quotes for the sidebar for context
         quotes = find_three_random_quotes()
         context["quotes"] = quotes
 
+        # Return the comments for current article.
+        # Displays in reverse datetime order
         comments = Comment.objects.filter(article_uuid=uuid).order_by('-datetime')
         context["comments"] = comments
 
+        # Return ALL THE THINGS!
         return context
 
     def form_valid(self, form):
+        # Collect and clean form data
         comment = form.cleaned_data["comment"]
         uuid = form.cleaned_data["article_uuid"]
 
+        # Create new Comment object and Save to DB
         new_comment = Comment(comment=comment, article_uuid=uuid)
         new_comment.save()
 
+        # Return the user to the current article page
         return HttpResponseRedirect(reverse('article_page', kwargs={'article_uuid': uuid}))
